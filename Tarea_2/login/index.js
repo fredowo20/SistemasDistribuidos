40babe04d763e0bb0889c8c29e0d95307e99f1a1
fredowo20 <103700122+fredowo20@users.noreply.github.com>
2022-05-24 00:17:11 -0400

    Update index.js

--- Tarea_2/login/index.js
+++ Tarea_2/login/index.js
@@ -17,7 +17,7 @@ const { Kafka } = require('kafkajs')
 
 const kafka = new Kafka({
   clientId: 'my-producer',
-  brokers: ['localhost:9092'/*, 'localhost:9093', 'localhost:9094'*/]
+  brokers: [process.env.kafkaHost/*, 'localhost:9093', 'localhost:9094'*/]
 })
 
 const producer = kafka.producer()
